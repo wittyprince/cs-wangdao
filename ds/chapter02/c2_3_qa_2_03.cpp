ef5905fb3c1a0a41b3c5be733330b760d662d9e9
wangchen <516760573@qq.com>
2024-07-16 22:21:26 +0800

    ds-2.3-qa_2_03-将带头结点的链表就地逆置

---
+++ ds/chapter02/c2_3_qa_2_03.cpp
@@ -0,0 +1,41 @@
+//
+// Created by 51676 on 2024/7/16.
+//
+
+#include <stdio.h>
+#include "LNode.h"
+
+// 头插法 reverse
+bool reverseLinkedList(LinkedList &L) {
+    LNode *p = L->next; // 工作指针，相当于游标
+    LNode *q; // 用于接收p的后继节点, 防止断链
+    L->next = NULL;
+    while (p != NULL) {
+        q = p->next; // 用于接收p的后继节点, 防止断链
+        // 头插法, 把p插入到链表头部
+        p->next = L->next;
+        L->next = p;
+        // 游标后移
+        p = q;
+    }
+
+    return true;
+}
+
+
+int main() {
+
+    LinkedList L;
+    initLinkedList2(L);
+    for (int i = 1; i < 6; ++i) {
+        headInsert(L, i);
+    }
+    printLinkedList(L);
+    printf("1===\n");
+
+    reverseLinkedList(L);
+    printLinkedList(L);
+
+
+    return 0;
+}
