92db5d8a487fd39960fd9082724173a9bed1fe46
wangchen <516760573@qq.com>
2024-07-16 22:44:31 +0800

    ds-2.3-qa_2_04-删除链表中某范围内的所有元素-单指针法 vs 双指针法

---
+++ ds/chapter02/c2_3_qa_2_04.cpp
@@ -0,0 +1,60 @@
+//
+// Created by 51676 on 2024/7/16.
+//
+
+#include <stdio.h>
+#include "LNode.h"
+
+bool deleteBetween(LinkedList &L, ElementType a, ElementType b) {
+
+//    LNode *p = L->next; // 工作指针, 相当于游标
+    LNode *prior = L;
+
+    while (prior != NULL && prior->next != NULL) {
+        LNode *p = prior->next;
+        if (p->data >= a && p->data <= b) {
+            prior->next = p->next;
+            free(p);
+        } else {
+            prior = p;
+        }
+    }
+    return true;
+}
+
+bool deleteBetween2(LinkedList &L, ElementType a, ElementType b) {
+
+    LNode *p = L->next; // 工作指针, 相当于游标
+    LNode *prior = L;
+
+    while (prior != NULL && p != NULL) {
+        LNode *q = p;
+        if (p->data >= a && p->data <= b) {
+            prior->next = p->next;
+            free(q);
+            p = prior->next;
+        } else {
+            prior = p;
+            p = p->next;
+        }
+    }
+    return true;
+}
+
+
+int main() {
+
+    LinkedList L;
+    initLinkedList2(L);
+    for (int i = 1; i < 6; ++i) {
+        headInsert(L, i);
+    }
+    printLinkedList(L);
+    printf("1===\n");
+
+    deleteBetween2(L, 1, 3);
+    printLinkedList(L);
+
+
+    return 0;
+}
