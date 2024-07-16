232c05a482add976b6628b73d85d7c8e5f8ceac7
wangchen <516760573@qq.com>
2024-07-17 00:05:23 +0800

    ds-2.3-qa_2_07-有序单链表中删除重复元素

---
+++ ds/chapter02/c2_3_qa_2_07.cpp
@@ -0,0 +1,45 @@
+//
+// Created by 51676 on 2024/7/16.
+//
+
+#include <stdio.h>
+#include "LNode.h"
+
+void deleteDuplicate(LinkedList &L) {
+    LNode *prior = L;
+    LNode *p;
+    while (prior != NULL && prior->next != NULL) {
+        p = prior->next;
+        if (prior->data == p->data) {
+            prior->next = p->next;
+            free(p);
+        } else {
+            prior = prior->next;
+        }
+    }
+
+}
+
+int main() {
+
+    LinkedList L;
+    initLinkedList2(L);
+    tailInsert(L, 7);
+    tailInsert(L, 10);
+    tailInsert(L, 10);
+    tailInsert(L, 21);
+    tailInsert(L, 30);
+    tailInsert(L, 42);
+    tailInsert(L, 42);
+    tailInsert(L, 42);
+    tailInsert(L, 51);
+    tailInsert(L, 70);
+    printLinkedList(L);
+    printf("1===\n");
+
+    deleteDuplicate(L);
+    printLinkedList(L);
+
+
+    return 0;
+}
