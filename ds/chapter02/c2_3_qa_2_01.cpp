4642ff8b64ed35b9b36de2749fc6280bbe1362b0
wangchen <516760573@qq.com>
2024-07-16 21:01:55 +0800

    ds-2.3-qa_2_01-删除链表中所有值为x的节点

---
+++ ds/chapter02/c2_3_qa_2_01.cpp
@@ -0,0 +1,45 @@
+//
+// Created by 51676 on 2024/7/15.
+//
+#include <stdio.h>
+
+#include "LNode.h"
+
+// 删除链表中所有值为e的节点
+bool deleteTheElement(LinkedList &L, ElementType e) {
+    LNode *prior = L;
+    while (prior != NULL && prior->next != NULL) {
+        LNode *p = prior->next;
+        if (p->data == e) {
+            prior->next = p->next;
+            free(p);
+        } else {
+            prior = prior->next;
+        }
+    }
+    return true;
+}
+
+int main() {
+
+    LinkedList L;
+    initLinkedList2(L);
+//    int length = lengthOfLinkedList(L);
+//    printf("%3d\n", length);
+    headInsert(L, 1);
+    headInsert(L, 2);
+    headInsert(L, 3);
+    headInsert(L, 3);
+    headInsert(L, 5);
+
+    printLinkedList(L);
+    printf("1===\n");
+    deleteTheElement(L, 3);
+    printLinkedList(L);
+
+
+
+
+
+    return 0;
+}
