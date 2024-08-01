a4ee12e8a5378f1ca9ca1e1b9fe9d2b11e0bca4f
wangchen <516760573@qq.com>
2024-08-01 11:25:49 +0800

    ds-5-非线性结构-树-树的孩子表示法-顺序存储+链式存储-增加注释

--- ds/chapter05/ChildTree.h
+++ ds/chapter05/ChildTree.h
@@ -8,12 +8,12 @@
 // ref: https://www.cnblogs.com/desola/p/12707029.html
 
 // 树的孩子表示法 - 顺序存储 + 链式存储
+//      也可以用于表示森林, 但是需要改造,
+//          即要保存多个根节点的位置rootIdx1, rootIdx2...
+//              但是数组及数组长度可以沿用
 
 #define ChildTreeMaxSize 10
-
 typedef int ElementType;
-
-// 数的节点的定义
 
 // 孩子链表的孩子节点
 typedef struct childTreeNode {
