4e58a9a60e6ce86be07c3a71bdcab252410d709d
wangchen <516760573@qq.com>
2024-08-01 10:23:56 +0800

    ds-5-非线性结构-树-树的双亲表示法 - 顺序存储-增加注释

--- ds/chapter05/ParentTree.h
+++ ds/chapter05/ParentTree.h
@@ -6,6 +6,7 @@
 #define CS_WANGDAO_PARENTTREE_H
 
 // 树的双亲表示法 - 顺序存储
+//      也可以用于表示森林
 //  注意: 与二叉树的顺序存储的区别
 //  树的顺序存储结构中, 数组下标代表节点的标号, 下标中所存的parent指示了节点之间的关系。
 //  二叉树的顺序存储结构中, 数组下标既代表了节点的编号, 又指示了二叉树中各节点之间的关系。
@@ -13,14 +14,16 @@
 //  但, 树却不能用二叉树的存储结构来存储。
 #define ParentTreeMaxSize 10
 typedef int ElementType;
+// 树的节点的定义
 typedef struct parentTreeNode {
     ElementType data; // 数据域
-    int parent; // parent的下标
+    int parent; // parent的下标, 双亲位置域
 } ParentTreeNode;
 
+// 树的类型定义
 typedef struct parentTree {
     ParentTreeNode data[ParentTreeMaxSize]; // 数组
-    int length; // 数组的长度
+    int length; // 数组的长度, 即节点数
 } ParentTree;
 
 #endif //CS_WANGDAO_PARENTTREE_H
