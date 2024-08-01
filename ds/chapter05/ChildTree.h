48dbac2cac01e2e3dd97d1d338abac8dfb1dd30a
wangchen <516760573@qq.com>
2024-08-01 11:20:16 +0800

    ds-5-非线性结构-树-树的孩子表示法-顺序存储+链式存储

---
+++ ds/chapter05/ChildTree.h
@@ -0,0 +1,39 @@
+//
+// Created by 51676 on 2024/8/1.
+//
+
+#ifndef CS_WANGDAO_CHILDTREE_H
+#define CS_WANGDAO_CHILDTREE_H
+
+// ref: https://www.cnblogs.com/desola/p/12707029.html
+
+// 树的孩子表示法 - 顺序存储 + 链式存储
+
+#define ChildTreeMaxSize 10
+
+typedef int ElementType;
+
+// 数的节点的定义
+
+// 孩子链表的孩子节点
+typedef struct childTreeNode {
+//    ElementType data; // 数据域-数据元素
+//    int firstChild; // 第一个孩子节点的位置索引
+    int childIndex; // 孩子节点在数组中的位置 - 数据域, 即用来存储某个节点在 <b>表头数组</b> 中的下标
+    struct childTreeNode *next; // 下一个孩子 - 指针域, 用来存储该节点的下一个孩子节点的指针
+} ChildTreeNode;
+
+// 表头数组的(表头)节点
+typedef struct childTreeBox {
+    ElementType data; // 数据域-存储数据信息
+    ChildTreeNode *firstChild; // 第一个孩子节点的指针 - 头指针域, 用来存储该节点的孩子链表的头指针
+} ChildTreeBox;
+
+// 树结构
+typedef struct childTree {
+    ChildTreeBox box[ChildTreeMaxSize]; // 节点数组
+    int length; // 数组长度, 即节点数
+    int rootIndex; // 根节点的位置
+} ChildTree;
+
+#endif //CS_WANGDAO_CHILDTREE_H
