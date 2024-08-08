853440b5129fb93877e241058ba6efe39d1722a0
wangchen <516760573@qq.com>
2024-08-08 17:29:09 +0800

    ds-7-算法-查找-二叉查找树-插入-递归方式

--- ds/chapter07/BST_Search.cpp
+++ ds/chapter07/BST_Search.cpp
@@ -12,6 +12,78 @@ typedef struct bstNode {
     struct bstNode *left, *right;
 } BSTNode, *BST;
 
+// 链表节点
+typedef struct bstLinkedNode {
+    BSTNode *linkedNodeData;
+    struct bstLinkedNode *linkedNodeNext;
+} BSTLinkedNode, *BSTLinkedNodeList;
+
+
+void visitBSTNode(BSTNode *node) {
+    printf("%3d", node->data);
+}
+
+void levelOrder(BST root) {
+    // 构造链表, 用于树的辅助遍历
+    // 初始化带头结点的链表
+    BSTLinkedNodeList linkedList = (BSTLinkedNodeList) malloc(sizeof(BSTLinkedNode));
+    linkedList->linkedNodeData = 0;
+    linkedList->linkedNodeNext = NULL;
+    // 声明头结点
+    BSTLinkedNode *head = linkedList;
+    // 尾结点: 用于尾插法插入链表
+    BSTLinkedNode *tail = linkedList;
+    // 当前节点, 用于遍历
+    BSTLinkedNode *current;
+
+    // 构造根节点
+    BSTLinkedNode *tmpLinkedNode = (BSTLinkedNode *) malloc(sizeof(BSTLinkedNode));
+    tmpLinkedNode->linkedNodeData = root;
+    tmpLinkedNode->linkedNodeNext = NULL;
+    // 根节点 插入链表
+    tail->linkedNodeNext = tmpLinkedNode;
+    tail = tail->linkedNodeNext;
+
+    // 遍历链表
+    current = head->linkedNodeNext;
+    while (current != NULL) {
+        BSTNode *currentBSTNode = current->linkedNodeData;
+        if (currentBSTNode != NULL) {
+            // 遍历树节点
+            visitBSTNode(currentBSTNode);
+
+            // 把左、右孩子插入链表
+            if (currentBSTNode->left != NULL) {
+                // 构造节点
+                tmpLinkedNode = (BSTLinkedNode *) malloc(sizeof(BSTLinkedNode));
+                tmpLinkedNode->linkedNodeData = currentBSTNode->left;
+                tmpLinkedNode->linkedNodeNext = NULL;
+                // 节点 插入链表
+                tail->linkedNodeNext = tmpLinkedNode;
+                tail = tail->linkedNodeNext;
+            }
+            if (currentBSTNode->right != NULL) {
+                // 构造节点
+                tmpLinkedNode = (BSTLinkedNode *) malloc(sizeof(BSTLinkedNode));
+                tmpLinkedNode->linkedNodeData = currentBSTNode->right;
+                tmpLinkedNode->linkedNodeNext = NULL;
+                // 节点 插入链表
+                tail->linkedNodeNext = tmpLinkedNode;
+                tail = tail->linkedNodeNext;
+            }
+        }
+        current = current->linkedNodeNext;
+    }
+}
+
+void inOrder(BST root) {
+    if (root != NULL) {
+        inOrder(root->left);
+        visitBSTNode(root);
+        inOrder(root->right);
+    }
+}
+
 // 二叉排序树-非递归方式
 BSTNode *BST_Search(BST T, ElementType e) {
     while (T != NULL && T->data != e) {
@@ -33,6 +105,26 @@ BSTNode *BST_Search_Recursive(BST T, ElementType e) {
         return BST_Search_Recursive(T->left, e);
     } else {
         return BST_Search_Recursive(T->right, e);
+    }
+}
+
+// 二叉排序树-插入-递归方式
+bool BST_Insert(BST &T, ElementType e) {
+    if (T == NULL) {
+        BSTNode *node = (BSTNode *) malloc(sizeof(BSTNode));
+        node->data = e;
+        node->left = node->right = NULL;
+        T = node;
+        return true;
+    }
+    if (T->data == e) {
+        printf("the value [%3d] has been already existed!", e);
+        return false;
+    }
+    if (T->data > e) {
+        return BST_Insert(T->left, e);
+    } else {
+        return BST_Insert(T->right, e);
     }
 }
 
@@ -67,11 +159,20 @@ int main() {
     BSTNode * node2;
     node2 = BST_Search_Recursive(n6, 3/*, node2*/);
     if (node2 != NULL) {
-        printf("%3d", node2->data);
+        printf("%3d\n", node2->data);
     } else {
         printf("search failed\n");
     }
 
+
+    BST root = NULL;
+    BST_Insert(root, 6);
+    BST_Insert(root, 2);
+    BST_Insert(root, 8);
+    BST_Insert(root, 1);
+    BST_Insert(root, 4);
+    BST_Insert(root, 3);
+    inOrder(n6);
 
     return 0;
 }
