7ccc43f449c7b9f7a6a4f18a79692ff2442408c5
wangchen <516760573@qq.com>
2024-08-09 10:22:51 +0800

    ds-7-算法-查找-二分查找-递归方式

--- ds/chapter07/BinarySearch.cpp
+++ ds/chapter07/BinarySearch.cpp
@@ -20,13 +20,30 @@ int binarySearch(int a[], int low, int high, int target) {
     return -1;
 }
 
+int binarySearchRecursive(int a[], int low, int high, int target) {
+    int mid = (low + high) / 2;
+    if (/*mid >=0 && */low <= high) {
+        if (a[mid] == target) {
+            return mid;
+        } else if (a[mid] > target) {
+            return binarySearchRecursive(a, low, mid - 1, target);
+        } else {
+            return binarySearchRecursive(a, mid + 1, high, target);
+        }
+    }
+    return -1;
+}
+
 
 int main() {
 
     int a[] = {-1,0,3,5,9,12};
 
     int target = binarySearch(a, 0, 5, 2);
-    printf("%d", target);
+    printf("%d\n", target);
+
+    int target2 = binarySearchRecursive(a, 0, 5, 3);
+    printf("%d\n", target2);
 
 
     return 0;
