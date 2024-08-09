ebce4c4232d3a010deb35261356d1fa317f8555a
wangchen <516760573@qq.com>
2024-08-09 14:37:04 +0800

    ds-7-算法-查找-简易版LRU-基于链式存储结构

--- ds/chapter07/LRU_Simple.cpp
+++ ds/chapter07/LRU_Simple.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 // 简易版LRU
 
@@ -30,12 +31,76 @@ void searchAndExchange(int a[], int length, int target) {
     }
 }
 
+// ======================================
+typedef struct linkedNode {
+    int data;
+    struct linkedNode *next;
+} LinkedNode, *LinkedList;
+
+void printLinkedList(LinkedList linkedList) {
+    LinkedNode *firstNode = linkedList->next;
+    while (firstNode != NULL) {
+        printf("%3d",firstNode->data);
+        firstNode = firstNode->next;
+    }
+    printf("\n");
+}
+
+void searchAndExchange2(LinkedList linkedList, int target) {
+    if (linkedList == NULL) {
+        return;
+    }
+    LinkedNode *previous = linkedList;
+    LinkedNode *current = linkedList->next;
+    if (current != NULL && current->data == target) {
+        return;
+    }
+    previous = current;
+    current = current->next;
+    while (current != NULL && current->data != target) {
+        previous = current;
+        current = current->next;
+    }
+    if (current != NULL) {
+        swap(previous->data, current->data);
+    }
+}
+// ======================================
+
 int main() {
     int a[] = {1, 2, 3, 4, 5};
     printArray(a, 5);
 
     searchAndExchange(a, 5, 6);
     printArray(a, 5);
+
+    printf("\n====================\n");
+
+    //==========================================
+    LinkedList linkedList = (LinkedList)malloc(sizeof(LinkedNode));
+    linkedList->data = 0;
+    linkedList->next = NULL;
+    LinkedNode *n1 = (LinkedNode *) calloc(1, sizeof(LinkedNode));
+    n1->data = 1;
+    LinkedNode *n2 = (LinkedNode *) calloc(1, sizeof(LinkedNode));
+    n2->data = 2;
+    LinkedNode *n3 = (LinkedNode *) calloc(1, sizeof(LinkedNode));
+    n3->data = 3;
+    LinkedNode *n4 = (LinkedNode *) calloc(1, sizeof(LinkedNode));
+    n4->data = 4;
+    LinkedNode *n5 = (LinkedNode *) calloc(1, sizeof(LinkedNode));
+    n5->data = 5;
+    linkedList->next = n1;
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n5;
+    n5->next = NULL;
+    printLinkedList(linkedList);
+    searchAndExchange2(linkedList, 6);
+    printLinkedList(linkedList);
+
+
 
     return 0;
 }
