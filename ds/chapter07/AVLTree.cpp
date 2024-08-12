fd495393f58203aeb91a6f07ed9e1b6566bbf16e
wangchen <516760573@qq.com>
2024-08-12 15:58:58 +0800

    ds-7-算法-查找-AVL平衡二叉树

---
+++ ds/chapter07/AVLTree.cpp
@@ -0,0 +1,18 @@
+//
+// Created by 51676 on 2024/8/9.
+//
+
+// 平衡二叉树 Balanced Binary Tree
+// AVL树
+
+typedef struct avlNode {
+    int key; // 数据域
+    struct avlNode *left, *right; // 左、右子树
+    int balance; // 平衡因子
+} AVLNode, *AVLTree;
+
+int main() {
+
+    return 0;
+}
+
