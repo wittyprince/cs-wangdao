4a742cb259d36ea5cf390fa795f78f36f034ce4b
wangchen <516760573@qq.com>
2024-08-13 15:54:09 +0800

    ds-7-算法-查找-LRU+LFU

---
+++ ds/chapter07/LFU.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 51676 on 2024/8/9.
+//
+
+// Least Frequently Use 最不常使用
+
+int main() {
+
+    return 0;
+}
---
+++ ds/chapter07/LRU.cpp
@@ -0,0 +1,18 @@
+//
+// Created by 51676 on 2024/8/9.
+//
+
+// Least Recently Use 最近最少使用
+/**
+ * ref: https://www.cnblogs.com/yanchuanbin/p/14791389.html
+ * ref: https://blog.csdn.net/weixin_45304503/article/details/136756074
+ * LRU算法
+ * 根据数据的历史访问记录来进行淘汰数据，
+ * 其核心思想是“如果数据最近被访问过，那么将来被访问的几率也更高”。
+ * 也就是说，当限定的空间已存满数据时，应当把最久没有被访问到的数据淘汰。
+ */
+
+int main() {
+
+    return 0;
+}
