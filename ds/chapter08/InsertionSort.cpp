e16a6b37b586b775cd8b83660052fe9a93e389ef
wangchen <516760573@qq.com>
2024-08-16 14:33:01 +0800

    ds-8-排序-插入排序-直接插入排序-增加注释+其他写法

--- ds/chapter08/InsertionSort.cpp
+++ ds/chapter08/InsertionSort.cpp
@@ -9,7 +9,7 @@
 
 void printArray(int a[], int length) {
     for (int i = 0; i < length; ++i) {
-        printf("%3d", a[i]);
+        printf("%3d,", a[i]);
     }
     printf("\n");
 }
@@ -29,6 +29,38 @@ void insertionSort(int a[], int length) {
                 a[j + 1] = a[j];
                 idx = j;
             }
+            // 如果使用下面的break, 可以减少比较次数
+//            else {
+//                break;
+//            }
+        }
+        a[idx] = tmp;
+    }
+}
+
+void insertionSort3(int a[], int length) {
+    int i, j, tmp, idx;
+    for (i = 0; i < length - 1; ++i) {
+        idx = i + 1;
+        tmp = a[idx];
+        for (j = i; j >= 0 && tmp < a[j]; j--) { // 遍历找到索引为i+1的元素需要插入的位置
+            a[j + 1] = a[j];
+            idx = j;
+        }
+        a[idx] = tmp;
+    }
+}
+
+void insertionSort2(int a[], int length) {
+    int i, j, tmp, idx;
+    for (i = 1; i < length; ++i) {
+        idx = i;
+        tmp = a[idx];
+        for (j = i; j >= 0; j--) { // 遍历找到索引为i+1的元素需要插入的位置
+            if (tmp < a[j]) {
+                a[j + 1] = a[j];
+                idx = j;
+            }
         }
         a[idx] = tmp;
     }
@@ -36,7 +68,9 @@ void insertionSort(int a[], int length) {
 
 int main() {
     int length = 8;
-    int a[8] = {3, 6, 7, 9, 2, 1, 10, 8};
+//    int a[8] = {3, 6, 7, 9, 2, 1, 10, 8};
+    int a[8] = {58, 11, 45, 87, 30, 63, 41, 32};
+//    int a[8] = {71, 30, 37, 39, 50, 24, 42, 67};
 //    srand(time(NULL));
 //    for (int i = 0; i < length; ++i) {
 //        a[i] = rand() % 100;
